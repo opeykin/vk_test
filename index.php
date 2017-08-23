6415ca89ddd8168fe97a72beae4389af520d6b77
alexander.opeykin <alexander.opeykin@gmail.com>
2017-08-23 20:34:09 +0200

    simple php+mysql test fetch

--- index.php
+++ index.php
@@ -15,7 +15,20 @@
 </head>
 <body>
 
+<?php
+$username = "dev";
+$pwd = "dev";
+$hostname = "localhost";
 
+$db = mysqli_connect($hostname, $username, $pwd) or die("Can't connect");
+mysqli_select_db($db, "vk_test") or die("Cant' select db");
+
+$result = mysqli_query($db, "SELECT id, price FROM items ORDER BY price LIMIT 10");
+while ($row = mysqli_fetch_row($result)) {
+    echo $row[0].": ".$row[1]."<br>";
+}
+
+?>
 
 <!--<script src="https://code.jquery.com/jquery-3.2.1.min.js"-->
 <!--integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>-->
