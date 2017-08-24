0394e9007c546ca6e0cffbe6ac4e428bb3cfe675
alexander.opeykin <alexander.opeykin@gmail.com>
2017-08-24 16:23:42 +0200

    Add paging. Use page get param. Add sorting param.

--- index.php
+++ index.php
@@ -17,6 +17,39 @@
 
 <?php
 
+function get_order()
+{
+    if (!isset($_GET['order']))
+        return 0;
+
+    if (is_numeric($_GET['order'])) {
+        return (int)$_GET['order'];
+    }
+
+    return 0;
+}
+
+// sorter with correct element selected
+// TODO: Look like shit. Do people even generate stuff like this?
+
+echo '<div class="clearfix">
+    <div class="fl_l">I want to see</div>
+    <select onchange="document.location.href=\'?order=\'+this.selectedIndex">';
+
+$order = get_order();
+$sort_items = array('Cheap', 'Expensive', 'Old', 'New');
+
+for ($i = 0; $i < count($sort_items); $i++) {
+    echo '<option ';
+    if ($i === $order)
+        echo 'selected';
+    echo ">$sort_items[$i]</option>";
+}
+
+echo '</select></div>';
+
+
+
 function db_connect($host = '', $user = '', $password = '', $database = '')
 {
     $handle = mysqli_connect($host, $user, $password, $database);
@@ -56,10 +89,53 @@ function rows_to_html($rows)
     echo '</div>';
 }
 
+
 $PAGE_SIZE = 50;
+
+
+function get_page()
+{
+    if (!isset($_GET['page']))
+        return 0;
+
+    $page = $_GET['page'];
+    if (!is_numeric($page)) {
+        error_log("Illegal GET param. page is not a number: $page");
+        return 0;
+    }
+
+    return (int)$page;
+}
+
+
+
+function get_sorting()
+{
+    $order = get_order();
+    switch ($order) {
+        case '0': // cheap
+            return array('price', 'ASC');
+        case '1': // expensive
+            return array('price', 'DESC');
+        case '2': // old
+            return array('id', 'ASC');
+        case '3': // new
+            return array('id', 'DESC');
+    }
+
+    error_log("Illegal GET param. order has unknown value: $order");
+
+    return array('price', 'ASC');
+}
+
+$skip = get_page() * $PAGE_SIZE;
+$sorting = get_sorting();
+$sort_column = $sorting[0];
+$sort_direction = $sorting[1];
+
 $handle = db_connect('localhost', 'dev', 'dev', 'vk_test2') or die('Can\'t connect');
 
-$result = mysqli_query($handle, "SELECT id, img, name, price, description  FROM items ORDER BY price LIMIT $PAGE_SIZE");
+$result = mysqli_query($handle, "SELECT id, img, name, price, description  FROM items ORDER BY $sort_column $sort_direction LIMIT $skip, $PAGE_SIZE");
 $rows = mysqli_fetch_all($result, MYSQLI_NUM);
 rows_to_html($rows);
 mysqli_close($handle);
