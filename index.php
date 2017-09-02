b29395cafee16c5a213e472f832e454374f4332e
alexander.opeykin <alexander.opeykin@gmail.com>
2017-09-02 23:50:13 +0300

    add page selector

--- index.php
+++ index.php
@@ -22,7 +22,39 @@ function row_to_html($row)
 </div>";
 }
 
-function rows_to_html($rows)
+function html_print_page_link($url, $order, $page, $text) {
+    $url_with_params = "$url?order=$order&page=$page";
+    echo "<a href='$url_with_params'>$text</a>";
+}
+
+function html_print_page_switcher($cur_page, $page_count, $order)
+{
+    echo '<div>';
+
+    $url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+    if ($cur_page > 0) {
+        html_print_page_link($url, $order, 0, '0');
+    }
+
+    if ($cur_page > 1) {
+        html_print_page_link($url, $order, $cur_page - 1, '...');
+    }
+
+    html_print_page_link($url, $order, $cur_page, $cur_page);
+
+    if ($cur_page < $page_count - 2) {
+        html_print_page_link($url, $order, $cur_page + 1, '...');
+    }
+
+    if ($cur_page < $page_count - 1) {
+        html_print_page_link($url, $order, $page_count - 1, $page_count - 1);
+    }
+
+    echo '</div>';
+}
+
+function html_print_rows($rows)
 {
     echo '<div>';
 
@@ -48,11 +80,20 @@ function get_page()
     return (int)$page;
 }
 
-
-function get_sorting()
+function get_order()
 {
     $order = (int)($_GET['order'] ?? 0);
+    if (!is_numeric($order) || $order < 0 || $order >= 3) {
+        error_log("Illegal GET param. order has unknown value: $order");
+        return 0;
+    }
 
+    return (int)$order;
+
+}
+
+function get_sorting($order)
+{
     switch ($order) {
         case '0': // cheap
             return array('price', 'ASC');
@@ -63,8 +104,6 @@ function get_sorting()
         case '3': // new
             return array('id', 'DESC');
     }
-
-    error_log("Illegal GET param. order has unknown value: $order");
 
     return array('price', 'ASC');
 }
@@ -87,8 +126,9 @@ function db_fetch_items_count($handle)
 include_once 'header.php';
 include 'sorting_selector.php';
 
-$skip = get_page() * Constants::PAGE_SIZE;
-$sorting = get_sorting();
+
+$order = get_order();
+$sorting = get_sorting($order);
 $sort_column = $sorting[0];
 $sort_direction = $sorting[1];
 
@@ -96,12 +136,15 @@ $sort_direction = $sorting[1];
 $db_config = parse_ini_file(Constants::DB_CONFIG_PATH);
 $handle = db_connect($db_config) or die('Can\'t connect');
 
-$rows = db_fetch_items($handle, $sort_column, $sort_direction, $skip);
 $items_count = db_fetch_items_count($handle);
+$page_count = (int)($items_count / Constants::PAGE_SIZE + 1);
+$page = max(min(get_page(), $page_count - 1), 0);
+$skip = $page * Constants::PAGE_SIZE;
 
-rows_to_html($rows);
+$rows = db_fetch_items($handle, $sort_column, $sort_direction, $skip);
 
-echo 'Pages: ' . (int)($items_count / Constants::PAGE_SIZE + 1);
+html_print_rows($rows);
+html_print_page_switcher($page, $page_count, $order);
 
 
 include_once 'footer.php';
