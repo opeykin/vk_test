d438b665d4a710526d87830269d1e581f8804549
alexander.opeykin <alexander.opeykin@gmail.com>
2017-09-03 14:02:11 +0300

    simplify sorting_selector.php with short php tags

--- sorting_selector.php
+++ sorting_selector.php
@@ -5,9 +5,9 @@ $order = (int)($_GET['order'] ?? 0);
 <div class="clearfix">
     <div class="fl_l">I want to see</div>
     <select onchange="document.location.href='?order='+this.selectedIndex">
-        <option <?php echo ($order == 0 ? 'selected' : '') ?>>Cheap</option>
-        <option <?php echo ($order == 1 ? 'selected' : '') ?>>Expensive</option>
-        <option <?php echo ($order == 2 ? 'selected' : '') ?>>Old</option>
-        <option <?php echo ($order == 3 ? 'selected' : '') ?>>New</option>
+        <option <?=$order == 0 ? 'selected' : ''?>>Cheap</option>
+        <option <?=$order == 1 ? 'selected' : ''?>>Expensive</option>
+        <option <?=$order == 2 ? 'selected' : ''?>>Old</option>
+        <option <?=$order == 3 ? 'selected' : ''?>>New</option>
     </select>
 </div>
