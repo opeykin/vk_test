f9dc3baed51034113a131bb77704a72709c76fb1
alexander.opeykin <alexander.opeykin@gmail.com>
2017-09-05 21:30:18 +0300

    item list page layout

--- parts/items.php
+++ parts/items.php
@@ -1,13 +1,13 @@
-<div>
+<div id="items_list">
     <?php foreach ($items as $item): ?>
         <div class='list_item clearfix'>
             <div class='list_photo image_wrapper fl_l'>
-                <img src="<?=$item['img']?>">
+                <img src="<?=empty($item['img']) ? 'img/placeholder.jpg' : $item['img']?>">
             </div>
             <div class='list_text_block'>
-                <div><?=$item['name']?></div>
-                <div><?=$item['price']?></div>
-                <div><?=$item['description']?></div>
+                <div class="list_title"><?=$item['name']?></div>
+                <div class="list_price"><?=$item['price'] . ' $'?></div>
+                <div class="list_description crop"><?=$item['description']?></div>
             </div>
         </div>
     <?php endforeach ?>
