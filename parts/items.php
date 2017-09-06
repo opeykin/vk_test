24718e1177224424f92525f0a99479647ee08a33
alexander.opeykin <alexander.opeykin@gmail.com>
2017-09-06 20:08:23 +0300

    open item.php on image click

--- parts/items.php
+++ parts/items.php
@@ -2,7 +2,9 @@
     <?php foreach ($items as $item): ?>
         <div class='list_item clearfix'>
             <div class='list_photo image_wrapper fl_l'>
-                <img src="<?=empty($item['img']) ? 'img/placeholder.jpg' : $item['img']?>">
+                <a href="<?='item.php?id='.$item['id']?>">
+                    <img src="<?=empty($item['img']) ? 'img/placeholder.jpg' : $item['img']?>">
+                </a>
             </div>
             <div class='list_text_block'>
                 <a href="<?='item.php?id='.$item['id']?>" class="list_title"><?=$item['name']?></a>
