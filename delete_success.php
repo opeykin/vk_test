a495299b07e103ab49e5176357c380cd959eff9b
alexander.opeykin <alexander.opeykin@gmail.com>
2017-09-06 20:03:44 +0300

    auto redirect from delete_success.php to index.php

--- delete_success.php
+++ delete_success.php
@@ -1,4 +1,6 @@
-<div>
-    Success <a href="index.php">Back to items</a>
-</div>
+<?php
 
+include 'templates/header.php';
+header('refresh:2;url=index.php');
+echo 'Success <a href="index.php">Back to items</a>';
+include 'templates/footer.php.php';
